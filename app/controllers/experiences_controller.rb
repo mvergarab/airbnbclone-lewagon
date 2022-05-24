53b2cd9d2dc165967e1104331f083ed013e32c5d
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-05-24 15:07:03 +0200

    created controller for experience and index, show, create, new actions are defined

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,4 +1,33 @@
 class ExperiencesController < ApplicationController
   def index
+    @experiences = Experience.all
+  end
+
+  def show
+    @experience = Experience.find(params[:id])
+  end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @experience = Experience.new(review_params)
+    @experience.user = current_user
+    if @experience.save
+      redirect_to user_experiences_path(current_user), notice: 'Experience was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+
+  def review_params
+    params.require(:experience).permit(:name, :description, :price)
   end
 end
