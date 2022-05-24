99d93dded74014be86e3919ee4b4611fc83e7d21
Kaviya Dhanapal <kaviya.dhpl@gmail.com>
2022-05-24 16:10:19 +0200

    log in works fin

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "experiences#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resource :user do
-    resources :experiences
-  end
+  root to: 'experiences#index'
+  resources :experiences
 end
